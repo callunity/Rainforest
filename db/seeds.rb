dac877425dec34d223980d8b2a7a2116c6dec67e
callunity <heather.e.armstrong@gmail.com>
2014-08-29 16:09:50 -0400

    Generated product controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!(:name => "Chair", :description => "Has legs", :price_in_cents => 33000)
+Product.create!(:name => "Column vaporizer", :description => "Gets rid of all of those annoying view-blocking columns", :price_in_cents => 4000000000)
