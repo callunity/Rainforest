c7ed638543b3b91f3cfe6b7062ba01882cae3169
callunity <heather.e.armstrong@gmail.com>
2014-09-02 14:52:52 -0400

    Fixed login display and cleaned up Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to products_path, notice: "Signed up!"
+      flash[:notice] = "Signed up!"
+      redirect_to products_path
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true
+  validates :password, length: 8..20
 end
